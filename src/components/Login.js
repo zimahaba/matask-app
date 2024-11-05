bf1e15b7b86e17556c89e4ed1e441ae32d71617d
zimahaba <pmaquitorres@gmail.com>
2024-11-05 07:53:14 -0300

    login page bg color

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,25 +14,27 @@ const Login = () => {
     }
 
     return (
-        <div className="container login-container">
-        
-            <div className="card p-4" style={{width: '100%', maxWidth: '400px'}}>
-                <h3 className="text-center mb-4">Login</h3>
-                <form onSubmit={loginHandler}>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" ref={usernameInputRef} placeholder="Enter email" required/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" ref={passwordInputRef} placeholder="Password" required/>
-                    </div>
-                    <div className="mb-3 text-end">
-                        <a href="#" className="text-decoration-none">Forgot password?</a>
-                    </div>
-                    <button className="btn btn-primary w-100" onClick={loginHandler}>Login</button>
-                </form>
-            </div>  
+        <div style={{backgroundColor: '#183758'}}>
+            <div className="container login-container">
+            
+                <div className="card p-4 c-1" style={{width: '100%', maxWidth: '400px'}}>
+                    <h3 className="text-center mb-4">Login</h3>
+                    <form onSubmit={loginHandler}>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="email" ref={usernameInputRef} placeholder="Enter email" required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="password" ref={passwordInputRef} placeholder="Password" required/>
+                        </div>
+                        <div className="mb-3 text-end">
+                            <a href="#" className="text-decoration-none">Forgot password?</a>
+                        </div>
+                        <button className="btn btn-1 w-100" onClick={loginHandler}>Login</button>
+                    </form>
+                </div>  
+            </div>
         </div>
     );
 }
